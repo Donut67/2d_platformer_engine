5160b0a50d182806bd1ad70f584cf25d015d1be7
Donut67 <72974262+Donut67@users.noreply.github.com>
2023-07-07 12:47:46 +0200

    a

--- src/main/engine.cpp
+++ src/main/engine.cpp
@@ -1,5 +1,8 @@
+#define CR_HOST // required in the host only and before including cr.h
+#include "cr.h"
+
 #include "raylib.h"
-#include "../include/platformerEngineUI.h"
+#include "platformerEngineUI.h"
 #include <cstdlib>
 #include <iostream>
 #include <functional>
@@ -11,27 +14,9 @@ int main(){
     InitWindow(0, 0, "2D Platformer Engine");
     SetExitKey(KEY_NULL);
     MaximizeWindow();
-
-    // shared_ptr<string> text = make_shared<string>("This is an input text");
-    // shared_ptr<EngineObject> screen_object = make_shared<EngineInput<string>>(Vector2{0.0f, 0.0f}, Vector2{300.0f, 20.0f}, text);
     
     Scene scene("resources/prova.meta");
     EngineBaseEditor base_editor(scene);
-
-    // Nary<pair<bool, shared_ptr<GameObject>>> hierarchyTree = convertToNewTree(*scene.getRoot());
-    // vector<pair<float, pair<string, function<void()>>>> action_list = NaryToActionList(hierarchyTree);
-    // shared_ptr<EngineObject> screen_object = make_shared<EngineFitter>(Vector2{0.0f, 0.0f}, Vector2{(float)GetScreenWidth(), (float)GetScreenHeight()}, Padding{0, 0, 0, 0}, 0, false);
-    // {
-    //     shared_ptr<EngineObject> new_object = make_shared<EngineFitter>(Vector2{0.0f, 0.0f}, Vector2{300.0f, 0.0f}, Padding{1, -1, -1, 1}, -1);
-    //     screen_object->addObject(new_object);
-    //     new_object->addObject(EngineHierarchy::getInstance());
-    //     EngineHierarchy::getInstance()->setRoot(scene.getRoot());
-    // }
-    // {
-    //     shared_ptr<EngineObject> new_object = make_shared<EngineFitter>(Vector2{0.0f, 0.0f}, Vector2{300.0f, 0.0f}, Padding{1, 1, -1, -1}, -1);
-    //     screen_object->addObject(new_object);
-    //     new_object->addObject(EngineInspector::getInstance());
-    // }
 
     SetTargetFPS(60);
 
